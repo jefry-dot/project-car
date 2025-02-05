f9bbef41f6255b03467eeeda0d923fbe5feae17c
Salomo Jefry <salomojefry@example.com>
2025-02-05 12:21:06 +0700

    Routes laravel basic

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,117 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    $person = [
-        'name' => 'John Doe',
-        'email' => 'n6OYI@example.com',
-    ];
+    // $aboutPageUrl = '/about';
 
-    return view('welcome', compact('person'));
+    // $aboutPageUrl = route('about');
+    // dd($aboutPageUrl);
+
+    // $productUrl = route('product.view', ['lang' => 'en', 'id' => 1]);
+    // dd($productUrl);
+    return view('welcome');
 });
 
-Route::get('/about', function () {  
-    return view('about');
+Route::view('/about', 'about')->name('about');
+
+Route::get('/sum/{num1}/{num2}', function (float $num1, float $num2) {
+    
+
+    $sum = $num1 + $num2;
+    return "Sum of $num1 and $num2 is $sum";
+})->whereNumber(['num1', 'num2']);
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* buat nanti contoh router 404
+Route::fallback(function () {
+    return 'fallback';
 });
+*/
+
+
+
+// Route::prefix('admin')->group(function () {
+//     Route::get('/users', function () {
+//         return '/admin/users';
+//     });
+// });
+
+// Route::name('admin')->group(function () {
+//     Route::get('/users', function () {
+//         return '/users';
+//     })->name('users');
+// });
+
+
+// Route::get('/user/profile', function () {
+
+// })->name('profile');
+
+
+// Route::get('/current-user', function () {
+//     // return  redirect()->route('profile');
+
+//     return to_route('profile');
+// });
+
+// // Named Routes with Parameters
+
+// Route::get("/{lang}/product/{id}", function (string $lang, string $id) {
+    
+// })->name("product.view");
+
+
+
+
+/* Route Optional Parameter
+Route::get('/product/{category?}', function(string $category = null )
+{
+    return "Product from category=$category";
+});
+*/
+
+/* Route Parameter Validation
+Route::get('/user/{username}', function (string $username) {
+    // biar username harus huruf kecil
+})->where('username','[a-z]+');
+
+
+Route::get("{lang}/product/{id}", function (string $lang, string $id) {
+    // biar id harus angka dan minimal 4 angka
+})->where(['lang' => '[a-z]{2}', 'id' => '\d{4,}']);
+
+Route::get('/search/{search}', function (string $search) {
+    // biar search harus string    
+return $search;
+})->where('search', '.+');
+
+
+Route::get('/product/{id}', function (string $id) {
+    // biar id harus angka    
+return "Product with id=$id";
+})->WhereNumber("id");
+*/
+
+
+/*view registered Routes with artisan
+    php artisan route:list
+    php artisan route:list -v
+    php artisan route:list --except-vendor
+    php artisan route:list --only-vendor
+    php artisan route:list --path=api
+*/
+
+/*
+php artisan route:cache
+php artisan route:clear
+*/
