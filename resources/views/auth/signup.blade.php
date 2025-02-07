1991f5072b74d997ccca8abd2a098efad414dd2a
Salomo Jefry <salomojefry@example.com>
2025-02-07 21:48:13 +0700

    component 2

--- resources/views/auth/signup.blade.php
+++ resources/views/auth/signup.blade.php
@@ -1,18 +1,5 @@
-@extends('layouts.base', ['cssClass' => 'page-signup'])
-
-@section('title', 'Signup')
-
-@section('childContent')
-<main>
-    <div class="container-small page-login">
-      <div class="flex" style="gap: 5rem">
-        <div class="auth-page-form">
-          <div class="text-center">
-            <a href="/">
-              <img src="/img/logoipsum-265.svg" alt="" />
-            </a>
-          </div>
-          <h1 class="auth-page-title">Signup</h1>
+<x-guest-layout title="Signup" bodyClass="page-signup">
+  <h1 class="auth-page-title">Signup</h1>
 
           <form action="" method="post">
             <div class="form-group">
@@ -35,32 +22,15 @@
               <input type="text" placeholder="Phone" />
             </div>
             <button class="btn btn-primary btn-login w-full">Register</button>
-
-            <div class="grid grid-cols-2 gap-1 social-auth-buttons">
-              <button
-                class="btn btn-default flex justify-center items-center gap-1"
-              >
-                <img src="/img/google.png" alt="" style="width: 20px" />
-                Google
-              </button>
-              <button
-                class="btn btn-default flex justify-center items-center gap-1"
-              >
-                <img src="/img/facebook.png" alt="" style="width: 20px" />
-                Facebook
-              </button>
-            </div>
-            <div class="login-text-dont-have-account">
-              Already have an account? -
-              <a href="/login.html"> Click here to login </a>
-            </div>
           </form>
-        </div>
-        <div class="auth-page-image">
-          <img src="/img/car-png-39071.png" alt="" class="img-responsive" />
-        </div>
-      </div>
-    </div>
-  </main>
-
-@endsection
+            
+          
+          
+      
+      <x-slot:footerLinks>
+     
+      Already have an account? -
+      <a href="/login.html"> Click here to login </a>
+      </x-slot:footerLinks>
+      
+</x-guest-layout>
