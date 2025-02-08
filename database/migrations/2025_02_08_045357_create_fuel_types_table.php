040640d0f8c0923efc290bbb84ccdb4926ba9cf8
Salomo Jefry <salomojefry@example.com>
2025-02-08 17:20:23 +0700

    laravel migration

---
+++ database/migrations/2025_02_08_045357_create_fuel_types_table.php
@@ -0,0 +1,29 @@
+<!-- php artisan make:migration create_fuel_types_table -->
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //deklarasi table
+        Schema::create('fuel_types', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 45);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('fuel_types');
+    }
+};
